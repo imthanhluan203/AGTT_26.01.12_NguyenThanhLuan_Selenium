03aa8c001a85dd2fd53b3de00a572a48aaab035b
Nguyen Thanh Luan <imthanhluan203@gmail.com>
2026-02-04 22:13:08 +0700

    Login Logout TC1-TC4

---
+++ MyLearningSpace/Day3/Lesson1.java
@@ -0,0 +1,14 @@
+package Day3;
+
+import org.testng.annotations.Test;
+
+public class Lesson1 {
+	@Test
+	public void MobileLogin() {
+		System.out.println("MobileLogin");
+	}
+	@Test 
+	public void WebLogin() {
+		System.out.println("WebLogin");
+	}
+}
---
+++ MyLearningSpace/Day3/Lesson2.java
@@ -0,0 +1,14 @@
+package Day3;
+
+import org.testng.annotations.Test;
+
+public class Lesson2 {
+	@Test
+	public void MobileLogout() {
+		System.out.println("MobileLogout");
+	}
+	@Test 
+	public void WebLogout() {
+		System.out.println("WebLogout");
+	}
+}
